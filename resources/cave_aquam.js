1e05b9c3a6d4cbf10491d03b8807f73eb1114491
Flavio Pace <flavio.pace@gmail.com>
2017-01-11 21:35:52 +0100

    Add geolocation feature

--- resources/cave_aquam.js
+++ resources/cave_aquam.js
@@ -554,6 +554,39 @@ map.on('singleclick', function(evt) {
     onSingleClick(evt);
 });
 
+var accuracyFeature = new ol.Feature();
+geolocation.on('change:accuracyGeometry', function() {
+    accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+var positionFeature = new ol.Feature();
+    positionFeature.setStyle(new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 6,
+      fill: new ol.style.Fill({
+        color: '#3399CC'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#fff',
+        width: 2
+      })
+    })
+}));
+
+geolocation.on('change:position', function() {
+    var coordinates = geolocation.getPosition();
+    map.getView().setCenter(coordinates);
+    positionFeature.setGeometry(coordinates ?
+        new ol.geom.Point(coordinates) : null);
+});
+
+new ol.layer.Vector({
+    map: map,
+    source: new ol.source.Vector({
+      features: [accuracyFeature, positionFeature]
+    })
+});
+
 /**
 * Currently drawn feature.
 * @type {ol.Feature}
